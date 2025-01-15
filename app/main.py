0931d004a2ef0a630b51f51a756a983eac62c138
Vathsala <vathsala.sachar@gmail.com>
2025-01-15 09:32:39 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,13 +7,27 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         ip_text = input()
+        if not ip_text:
+            continue
+        # split text to get command and arguments
+        split_text = ip_text.split()
+        if len(split_text) == 1:
+            print(f"{ip_text}: command not found")
+            continue
 
-        if ip_text == "exit 0":
+        command = split_text[0]
+        command_len = len(command)
+        args = ip_text[command_len+1:]
+
+        if command == "exit" and args == "0":
             break
-        elif ip_text.split()[0] == "echo":
-            command = ip_text.split()[0]
-            args = ip_text[5:]
+        elif command == "echo":
             print(f"{args}")
+        elif command == "type":
+            if args in ["exit", "echo", "type"]:
+                print(f"{args} is a shell builtin")
+            else:
+                print(f"{args}: not found")
         else:
             print(f"{ip_text}: command not found")
 
